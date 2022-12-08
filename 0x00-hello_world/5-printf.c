3741b333f6f123c187847a3191e248bcd2f84d9b
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-08 05:36:19 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 
  */
 int main(void)
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
