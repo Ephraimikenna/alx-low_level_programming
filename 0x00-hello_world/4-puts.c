f9443a089a30b0863964a729fab7771cb0fbb66e
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-08 05:19:17 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 /**
- 
-   * main - Entry point
- 
-    *
- 
-     * Return: Always 0 (Success)
-
-      */
-
+ * main -Entry points
+ * Return: Always 0
+ */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			
-
-	    return (0);
+	return (0);
 
 }
