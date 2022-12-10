cc472aa0bd682a19536cf6cd49fd785254e74645
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-10 14:44:55 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing in lowercase
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'));
+		{
+			putchar(ch);
+		}
+			ch++;
+		}
+
+		putchar('\n');
+
+		return (0);
+}
