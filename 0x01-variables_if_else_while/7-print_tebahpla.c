aa8fe14daef78b9c240dc221d56d34768c5680db
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-10 16:48:00 -0800

    print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'z';
+	while
+		(a >= 'c')
+		{
+			putchar(a);
+			a--;
+		}
+
+	putchar('\n');
+
+	return (0);
+}
+
