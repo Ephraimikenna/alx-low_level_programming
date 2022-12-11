6f7a7926feccf2a7fcd3998cb85ea82911997abc
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-10 16:18:24 -0800

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + 'o');
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
