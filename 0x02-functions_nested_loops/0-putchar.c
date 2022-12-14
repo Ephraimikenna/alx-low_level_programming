8b54f6d6c2891850b6bd67d07c1a965a77224fc3
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-14 02:57:03 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -9,15 +9,15 @@ int main(void)
 {
 
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+       	_putchar('p');
+      	_putchar('u');
+      	_putchar('t');
+     	_putchar('c');
+    	_putchar('h');
+   	_putchar('a');
+  	_putchar('r');
 
-	_putchar('\n')
+       	_putchar('\n');
 
 	return (0);
 
