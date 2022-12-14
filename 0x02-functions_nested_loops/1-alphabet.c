4ddc9cb3dbc00d301cb84c7e28b2e7864aef41fa
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-14 14:52:37 -0800

    1 first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - Entry point
  *
+ * Return: 0
  */
 
-int main(void)
+void print_alphabet(void)
+
 {
-	char c;
+	char c = 'a';
 
-	for (c = 'a'; c <= 'z'; c++)
-
-	_putchar(c);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
 
 	_putchar('\n');
 
