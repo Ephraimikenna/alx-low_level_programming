bc8bf68b23eed185224bc74335c652206e1d8190
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-15 17:10:29 -0800

    1 task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 		/**
 
 		printf("Infinite loop incoming :(\n");
+		*/
 
 		i = 0;
-		
-		 
-		 *while (i < 10)
-		 */
+
+		 while (i < 10)
+
 		 {
 
 		  putchar(i);
@@ -25,7 +25,7 @@ int main(void)
 		  }
 
 		printf("Infinite loop avoided! \\o/\n");
-		
+
 
 		return (0);
 }
