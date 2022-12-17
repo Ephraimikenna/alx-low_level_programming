ee34f519086b1305b5948f2510a917da920c467a
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-16 17:08:31 -0800

    header

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number from 0 to 9
+ * print_most_numbers - prints numbers between 0 to 9
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int c;
+	int ch;
 
-	for (c = 48; c <= 58; c++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (c != 50 && c !=52)
-			_putchar(c);
+		_putchar(ch);
 	}
 	_putchar('\n');
 }
