6b6816e19b0a1b8e56e51f2b3c91be608058003a
Ephraimikenna <ephraimikenna2003@gmail.com>
2022-12-17 00:09:44 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line -prints straight line n times.
+ * on: times straight line is printed.
+ * Return: void
+ */
+vod print_line(int n)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n);
+}
