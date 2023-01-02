9891ac44298cde25e481546966b083d040c0e668
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-02 14:46:35 -0800

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - length of the prefix
+ * @s: string to be searched
+ * @accept: prefix to be measured
+ *
+ * Return: number of bytes consist only byte of accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+		for (i = 0; *(accept + i); i++)
+		{
+			if (*(s + j) == *(accept + i))
+			break;
+	}
+	if (*(accept + i) == '\0')
+		break;
+	}
+	return (j);
+}
