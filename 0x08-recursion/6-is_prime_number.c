a7e6b333331ba6b560b702ea233f3a81c3b49fc9
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-04 18:22:31 -0800

    last

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int is_divisible(int n, int d);
+int is_prime_number(int n);
+/**
+ * is_divisible - prime number printer
+ * @n: number checked
+ * @d: divisor
+ * Return: if number is divisible - 0.
+ *	if not - 1.
+ */
+
+int is_divisible(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+}
