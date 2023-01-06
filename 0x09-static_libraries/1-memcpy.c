af55a47e2451f2b09ea8f20b7cc6ea39d0dee470
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-06 13:01:59 -0800

    correct

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -11,13 +11,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int i = 0;
 
-	for (; r < i; r++)
+	while (i < n)
 	{
-		dest[r] = src[r];
-		n--;
+		*(dest + i) = *(src + i);
+		n++;
 	}
 	return (dest);
 
