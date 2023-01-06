b462b36d6fbcf7d7e7239c70063769fabae2850b
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-06 15:33:55 -0800

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  main - print all arguments
+ *  @argc: count elements
+ *  @argv: take the values here
+ *  Return: return what was given
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[i]);
+	return (0);
+}
