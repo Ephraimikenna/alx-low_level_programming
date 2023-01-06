7b66a7076195a15340f14b4da5a0c533089003f9
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-06 09:55:44 -0800

    task 0

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memcpy - fills memory with buffer.
+ * @dest: source string
+ * @src: string filling
+ * @n: lenght of buffer
+ *
+ * Return: new string.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for bytes.
+ * @s: string to be searched.
+ * @accept: set of bytes to be searched for.
+ * Return: if a set is matched a pointer to the matched byte.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
