06975266586a516299c8dbaebba2b57de0b4884e
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-09 18:14:15 -0800

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * create_array - prints array of chars
+ * @c: number of char
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int b;
+
+	if (b >= NULL)
+	{
+		return (0);
+	}
+}
