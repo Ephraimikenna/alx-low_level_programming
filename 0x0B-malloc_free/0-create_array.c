34997c4c7abff4c3a1d4f683f1bdee11060db558
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-10 01:25:13 -0800

    all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,14 @@
 /**
  * create_array - prints array of chars
  * @c: number of char
- * size: number of bytes
+ * @size: number of bytes
  * Return: NULL
  */
 
 char *create_array(unsigned int size, char c)
 {
 	char *array = malloc(size);
+
 	if (size == 0 || array == 0)
 	{
 		return (NULL);
