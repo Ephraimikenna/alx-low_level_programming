1c113a72a46195693c3508f2b4493232c49ea4ed
Ephraimikenna <ephraimikenna2003@gmail.com>
2023-01-21 17:17:32 -0800

    all

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,9 +23,9 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[1].op)
+	while (ops[i].op)
 	{
-		if (strcmp(ops[1].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
